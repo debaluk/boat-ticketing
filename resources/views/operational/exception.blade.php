772d75eb226e980fc6ee64637a8b35fac4eebb41
Budiasa <debaluk@gmail.com>
2026-09-11 02:05:10 +0800

    Add exception operational page

---
+++ resources/views/operational/exception.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+<div class="page-head"><h1>Exception</h1><p>Monitoring kondisi operasional yang perlu ditindaklanjuti.</p></div>
+<div class="card"><div class="card-body"><div class="alert alert-info mb-0">Belum ada modul pencatatan exception khusus pada database. Halaman ini disiapkan sebagai entry point operasional dan akan dihubungkan ke log exception setelah struktur datanya tersedia.</div></div></div>
+@endsection
